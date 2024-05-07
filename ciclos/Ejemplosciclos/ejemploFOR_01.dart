99cadb62be2abf662ccbd3d4d3f4206fb255dfac
brahim10 <brahianamarin23@gmail.com>
2024-05-07 08:14:14 -0500

    se hicieron dos ejemplos de ciclos

---
+++ ciclos/Ejemplosciclos/ejemploFOR_01.dart
@@ -0,0 +1,6 @@
+void main(List<String> args){
+  print("imprimir los numeros del un al 5");
+  for(int i=1; i <=5; i++){
+    print("i: $i");
+  }
+}
---
+++ ciclos/Ejemplosciclos/ejemploFOR_02.dart
@@ -0,0 +1,6 @@
+void main(List<String> args){
+  print("imprime numeros del 0 al 25, de 2 en 2 ");
+  for(int i=0; i<=25; i=i+2){
+    print("i: $i");
+  }
+}
