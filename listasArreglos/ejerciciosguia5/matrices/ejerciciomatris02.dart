3978debf8d1ddde9f94eaa82c04c3f1120755a59
sebas <sebasocampo721@gmail.com>
2024-06-06 11:55:02 -0500

    se corriogio ejercicio2 de matrices

--- listasArreglos/ejerciciosguia5/matrices/ejerciciomatris02.dart
+++ listasArreglos/ejerciciosguia5/matrices/ejerciciomatris02.dart
@@ -4,6 +4,8 @@ void main(List<String> args) {
   List<List<double>> matriz = [];
   double suma = 0;
   int cantfilas = 10, cantcolumnas=10;
+  int numMayor = 0;
+  int posicion = -1;
   for (int i = 0; i < cantfilas; i++){
     List<double> fila = [];
     for (int j = 0; j < cantcolumnas; j++){
@@ -12,7 +14,18 @@ void main(List<String> args) {
     }   
     matriz.add(fila); 
   }
-
-  
-
+  for (var i = 0; i <matriz.length; i++){
+  for (var j = 0; j <matriz[0].length; j++){
+    if (matriz[i][j]>numMayor){
+      numMayor= matriz[i][j] as int;
+      }
+  if(posicion !=-1){
+    print("el numero se encuentra en la posicion $posicion");
+  }else{
+    print("el numero no se encontro");
+  }
+    }
+    
+  }
+    
 }
