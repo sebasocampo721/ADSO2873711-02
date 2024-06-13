041b091fff50d2ffda863b415f3b18e0ca791a58
sebasocampo721 <sebasocampo721>
2024-06-13 17:25:55 -0500

    se realiza parte del ejercicio 4 matrices

--- listasArreglos/ejerciciosguia5/matrices/ejerciciomatris04.dart
+++ listasArreglos/ejerciciosguia5/matrices/ejerciciomatris04.dart
@@ -5,26 +5,54 @@ void main(List<String> args) {
   /*La escuela Ignacio Barrera, desea que le desarrolles un juego para sus alumnos de primaria. El juego se llama "cuadro mágico", un cuadro mágico es aquel en que la suma de cada fila, la suma de cada columna y la suma de las diagonales es exactamente igual. El cuadro mágico es de dos dimensiones.Desarrollar un programa que pida al usuario las dimensiones deseadas para  el cuadro mágico (un arreglo  de  dos  dimensiones)  en  forma  de  cuadro  y  luego  le  pida  los  datos  para  llenar  el  arreglo. Después  el  programa  debe  calcular  la  suma  de  cada  fila,  de  cada columna  y  de  cada  diagonal  del cuadro,  si  todas  las  sumas  son  iguales  entonces  le  deberás  de  indicar  por  medio  de  un  mensaje "Felicidades  creaste  un  cuadro  mágico",  de  lo  contrario  le  deberás  de  decir  el  siguiente  mensaje: "Modifica los números en el cuadro para que lo hagas mágico".
   */
   // definicion variables
-   Map<String, int> produccionPorArticulo = {};
-  List<int> produccionPorTurno = [0, 0, 0]; 
-  for (int i = 0; i < 5; i++) {
-    print('Ingrese el nombre del artículo ${i + 1}:');
-    String nombreArticulo = stdin.readLineSync()!;
-    for (int turno = 0; turno < 3; turno++) {
-      print('Ingrese la producción del artículo ${i + 1} en el turno ${turno + 1}:');
-      int produccion = int.parse(stdin.readLineSync()!);
-      produccionPorTurno[turno] += produccion; 
+
+  print('Ingrese el número de filas del cuadro:');
+  int filas = int.parse(stdin.readLineSync()!);
+  print('Ingrese el número de columnas del cuadro:');
+  int columnas = int.parse(stdin.readLineSync()!);
+
+  // Crear el cuadro mágico como una lista de listas
+  List<List<int>> cuadroMagico = List.generate(filas, (_) => List.filled(columnas, 0));
+
+  // Solicitar al usuario los datos para llenar el cuadro
+  for (int i = 0; i < filas; i++) {
+    for (int j = 0; j < columnas; j++) {
+      print('Ingrese el valor para la posición [$i][$j]:');
+      cuadroMagico[i][j] = int.parse(stdin.readLineSync()!);
     }
-    produccionPorArticulo[nombreArticulo] = produccionPorTurno.reduce((a, b) => a + b);
-  }
-  String articuloMasProducido = produccionPorArticulo.keys.reduce((a, b) => produccionPorArticulo[a]! > produccionPorArticulo[b]! ? a : b);
-  print('\n--- Reporte de Producción ---');
-  produccionPorArticulo.forEach((articulo, produccion) {
-    print('Producción de $articulo: $produccion');
-  });
-  for (int turno = 0; turno < 3; turno++) {
-    print('Producción total en el turno ${turno + 1}: ${produccionPorTurno[turno]}');
-  }
-  print('Artículo con mayor producción: $articuloMasProducido');
   }
 
+  // Calcular la suma de cada fila, cada columna y cada diagonal
+  int sumaPrincipal = 0;
+  int sumaSecundaria = 0;
+  bool esCuadroMagico = true;
+
+  for (int i = 0; i < filas; i++) {
+    int sumaFila = 0;
+    int sumaColumna = 0;
+    for (int j = 0; j < columnas; j++) {
+      sumaFila += cuadroMagico[i][j];
+      sumaColumna += cuadroMagico[j][i];
+      if (i == j) {
+        sumaPrincipal += cuadroMagico[i][j];
+      }
+      if (i + j == filas - 1) {
+        sumaSecundaria += cuadroMagico[i][j];
+      }
+    }
+    if (i == 0) {
+      sumaPrincipal = sumaFila;
+    }
+    if (sumaFila != sumaPrincipal || sumaColumna != sumaPrincipal) {
+      esCuadroMagico = false;
+      break;
+    }
+  }
+
+  // Verificar si es un cuadro mágico
+  if (esCuadroMagico && sumaPrincipal == sumaSecundaria) {
+    print('¡Felicidades! Has creado un cuadro mágico.');
+  } else {
+    print('Modifica los números en el cuadro para que lo hagas mágico.');
+  }
+}
